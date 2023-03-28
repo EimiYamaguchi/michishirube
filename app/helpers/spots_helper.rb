d2bc966f30321d545deb4e22746ff4476e9f02d0
EimiYamaguchi <104329344+EimiYamaguchi@users.noreply.github.com>
2023-03-28 11:26:40 +0000

    写真投稿機能追加

--- app/helpers/spots_helper.rb
+++ app/helpers/spots_helper.rb
@@ -2,6 +2,9 @@ module SpotsHelper
   def marker_content(spot)
     html = ""
     html << "<strong>Name:</strong> #{spot.name}<br />"
-    return html.html_safe
+    if spot.photo.attached?
+      html << "<strong>Photo:<?strong> #{image_tag(spot.photo, width: '100%')}<br />"
+    end
+      return html.html_safe
   end
 end
